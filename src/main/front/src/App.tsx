c763686e7340354a28dcc4f9486b9c1a498067f6
지금이야 <cz2123zc@gmail.com>
2022-04-23 23:56:06 +0900

    styled component 기본세팅

--- src/main/front/src/App.tsx
+++ src/main/front/src/App.tsx
@@ -1,24 +1,29 @@
+import GlobalStyle from "./styles/GlobalStyle";
+import Theme from "./styles/Theme";
+import { ThemeProvider } from "./styles/ThemeComponents";
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </ThemeProvider>
     </div>
   );
 }
