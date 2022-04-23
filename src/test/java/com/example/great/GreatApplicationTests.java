ebdc757219137d44509a9e39cc133f40c438bd5a
지금이야 <cz2123zc@gmail.com>
2022-04-23 19:46:45 +0900

    Initial commit

---
+++ src/test/java/com/example/great/GreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.great;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
